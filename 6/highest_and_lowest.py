a07a3800f782e05d16e2c89c0e507809e91d39bc
Mike Veronie <mveronie@gmail.com>
2024-07-16 00:49:57 -0400

    adds highest-and-lowest kata

---
+++ 6/highest_and_lowest.py
@@ -0,0 +1,32 @@
+""" Highest and Lowest
+
+In this little assignment you are given a string of space separated numbers,
+and have to return the highest and lowest number.
+
+Examples:
+high_and_low("1 2 3 4 5")  # return "5 1"
+high_and_low("1 2 -3 4 5") # return "5 -3"
+high_and_low("1 9 3 4 -5") # return "9 -5"
+
+Notes:
+- All numbers are valid Int32, no need to validate them.
+- There will always be at least one number in the input string.
+- Output string must be two numbers separated by a single space, and highest number is first.
+"""
+
+
+def high_and_low(numbers):
+    """
+    Creates a list of ints (cast from chars), then returns
+    the max and min cast back as strings.
+    """
+    nums = [int(x) for x in numbers.split()]
+    return str(max(nums)) + " " + str(min(nums))
+
+
+def high_and_low_2(numbers):
+    """
+    Uses an f-string instead, and makes the space in `split` explicit.
+    """
+    numbers = [int(x) for x in numbers.split(" ")]
+    return f"{max(numbers)} {min(numbers)}"
