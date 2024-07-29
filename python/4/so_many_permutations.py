ead6836bb40692fbeef8ef21c446fc06d52cb11b
Mike Veronie <mveronie@gmail.com>
2024-07-29 19:48:57 -0400

    adds permutations kata

---
+++ python/4/so_many_permutations.py
@@ -0,0 +1,30 @@
+""" So Many Permutations
+
+In this kata, your task is to create all permutations of a non-empty input string
+and remove duplicates, if present.
+
+Create as many "shufflings" as you can!
+
+Examples:
+    With input 'a':
+    Your function should return: ['a']
+
+    With input 'ab':
+    Your function should return ['ab', 'ba']
+
+    With input 'abc':
+    Your function should return ['abc','acb','bac','bca','cab','cba']
+
+    With input 'aabb':
+    Your function should return ['aabb', 'abab', 'abba', 'baab', 'baba', 'bbaa']
+Note: The order of the permutations doesn't matter.
+"""
+
+from itertools import permutations as perm
+
+
+def permutations(string):
+    """
+    returning a set of permutations to restrict duplicates
+    """
+    return set("".join(p) for p in perm(string))
